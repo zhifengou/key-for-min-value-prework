a8c30311a5cbe3cfdc2810057ecf601d7163be21
zhifeng ou <zhifengou@hotmail.com>
2018-09-02 22:00:40 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   key = nil 
   value = nil 
   name_hash.each do |k, v|
-    if lowest_value == nil || v < value
+    if value == nil || v < value
       value = v 
       key = k 
     end 
